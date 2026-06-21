cd26a4816c17f067f766a2397b54f027ab40778f
Fabian S. <49322676+pattlebass@users.noreply.github.com>
2026-06-21 21:19:00 +0300

    Remove test files

--- test_project/main.gd
+++ test_project/main.gd
@@ -3,37 +3,22 @@ extends Node2D
 func _ready() -> void:
 	test_plugin_functionality()
 
+
 func test_plugin_functionality() -> void:
 	var stream := AudioStreamMidiSequencer.new()
-	stream.soundfont_path = "res://Sony Ericsson A100 DB2020 Synth 1.0.4(1).sf2"
-	stream.bpm = 80
+	stream.soundfont_path = "res://midi/VintageDreamsWaves-v2.sf2"
+	stream.bpm = 100
 	
-	#stream.schedule_midi_file_at_beat("res://samples/samples2/sample_3_1.mid", 4 * 0)
-	for i in 10:
-		stream.schedule_midi_file_at_beat("res://samples/samples2/sample_2_%s.mid" % (i+1), 4 * i)
-	
-	#for i in 32:
-		#stream.schedule_midi_file_at_beat("res://samples/samples2/sample_0_%s.mid" % (i+1), 4 * i)
+	stream.schedule_midi_file_at_beat("res://midi/Dance_GM.mid", 4 * 0)
 	
 	$AudioStreamPlayer.stream = stream
 	$AudioStreamPlayer.play()
-	
-	#var playback := $AudioStreamPlayer.get_stream_playback() as AudioStreamPlaybackMidi
-	#playback.set_bpm(180)
-	#playback.load_soundfont("res://Sony Ericsson A100 DB2020 Synth 1.0.4(1).sf2")
-	#playback.play_midi("res://Sony Ericsson MusicDJ.mid")
-	#playback.start()
 
-func _process(_delta: float) -> void:
-	if not $AudioStreamPlayer.playing:
-		return
-	var real:float = $AudioStreamPlayer.get_playback_position() + AudioServer.get_time_since_last_mix()
-	var beat := real * 80 / 60.0
-	var column := floori(beat / 4)
-	print(column)
-	#print(real - $AudioStreamPlayer.get_playback_position())
-	#return
-	#var tick: int = $AudioStreamPlayer.get_stream_playback().get_playback_position_ticks()
-	#var beat: int = tick / AudioStreamMidiSequencer.TICKS_PER_BEAT
-	#var column := beat / 4
-	#print(tick)
+
+#func _process(_delta: float) -> void:
+	#if not $AudioStreamPlayer.playing:
+		#return
+	#var real:float = $AudioStreamPlayer.get_playback_position() + AudioServer.get_time_since_last_mix()
+	#var beat := real * 80 / 60.0
+	#var column := floori(beat / 4)
+	#print(column)
